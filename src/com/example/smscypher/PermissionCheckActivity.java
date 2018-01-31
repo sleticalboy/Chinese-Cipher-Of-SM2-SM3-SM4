1c0b8e89beba7579eb601685ba23231f2ca0ed18
sleticalboy <sleticalboy@gmail.com>
2018-01-31 20:00:33 +0800

    查看代码，添加注释

---
+++ src/com/example/smscypher/PermissionCheckActivity.java
@@ -0,0 +1,13 @@
+package com.example.smscypher;
+
+import android.app.Activity;
+
+/**
+ * Created on 18-1-31.
+ *
+ * @author sleticalboy
+ * @version 1.0
+ * @description
+ */
+public class PermissionCheckActivity extends Activity {
+}
